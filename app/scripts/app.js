ff91711156dcea3030a93aaef5b5615b0db5aed0
Peter Braun <pbraun@redhat.com>
2018-04-27 13:26:46 +0200

    remove bootstrap feature

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,13 +64,6 @@
 
   app.value('staticResourcePrefix', '');
 
-  app.value('bootstrapedAppName', function uuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
-      return v.toString(16);
-    });
-  });
-
   app.constant('allVariantTypes', ['android', 'ios', 'windows_wns', 'adm']);
 
   app.value('allowCreateVariant', function( app, variantType ) {
