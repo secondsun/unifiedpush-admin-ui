c837616ec066720262886f352f6b10c166dc9f86
Summers Pittman <secondsun@gmail.com>
2020-04-02 11:33:36 -0400

    Splitting Wizard and Applications in Welcome

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import { Component } from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.css';
-
+import {
+  UnifiedPushAdminClient,
+  PushApplication,
+} from '@aerogear/unifiedpush-admin-client';
 import { Page } from '@patternfly/react-core';
 import { Welcome } from './landing/Welcome';
 import Header from './common/Header';
 
-function App() {
-  return (
+interface MyState {
+  applications?: PushApplication[];
+}
+
+export class App extends Component<{}, MyState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {};
+  }
+
+  readonly upsadm = new UnifiedPushAdminClient('http://localhost:9999');
+
+  componentDidMount = (): void => {
+    this.upsadm.applications.find().then(apps => {
+      this.setState({
+        applications: apps,
+      });
+    });
+  };
+
+  render = (): React.ReactElement => (
     <Page header={<Header />}>
-      <Welcome />
+      <Welcome applications={this.state.applications} />
     </Page>
   );
 }
