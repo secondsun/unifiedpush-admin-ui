c33c1f63193f581b6838cb0a3f126e86b35b8a7a
Massimiliano Ziccardi <mziccard@redhat.com>
2020-04-15 16:51:51 +0200

    Added application list page

--- src/landing/ApplicationList.tsx
+++ src/landing/ApplicationList.tsx
@@ -86,6 +86,7 @@ export class ApplicationList extends Component<Props, State> {
     );
 
     return (
+<<<<<<< HEAD
       <ApplicationListConsumer>
         {({ applications, refresh }): ReactNode => {
           return (
@@ -134,6 +135,79 @@ export class ApplicationList extends Component<Props, State> {
           );
         }}
       </ApplicationListConsumer>
+=======
+      <>
+        <Split>
+          <SplitItem>
+            <Title
+              headingLevel="h1"
+              size="3xl"
+              style={{ paddingTop: 40, paddingLeft: 25, paddingBottom: 20 }}
+            >
+              Applications
+            </Title>
+          </SplitItem>
+          <SplitItem isFilled />
+          <SplitItem>
+            <Button
+              variant="link"
+              icon={<PlusCircleIcon />}
+              style={{ paddingTop: 50, paddingLeft: 25, paddingBottom: 20 }}
+            >
+              Create Application
+            </Button>
+          </SplitItem>
+        </Split>
+        <DataList aria-label="Expandable data list example">
+          {this.props.apps.map(app => (
+            <DataListItem aria-labelledby={'item'} key={app.pushApplicationID}>
+              <DataListItemRow>
+                <DataListItemCells
+                  dataListCells={[
+                    <DataListCell isIcon key="icon">
+                      <div className={'app-icon'}>A</div>
+                    </DataListCell>,
+                    <DataListCell key="primary content">
+                      <div className="title">{app.name}</div>
+                      <div className="subtitle">
+                        <List variant={ListVariant.inline}>
+                          <ListItem className="subtitle">
+                            <Label
+                              text={`created by admin ${app.developer}`}
+                              icon={<UserIcon />}
+                            />
+                          </ListItem>
+                          <ListItem className="subtitle">
+                            <Label
+                              text={`${
+                                app.variants ? app.variants.length : 0
+                              } variants`}
+                              icon={'fa fa-code-branch'}
+                            />
+                          </ListItem>
+                          <ListItem className="subtitle">
+                            <Label
+                              text={'0 messages sent'}
+                              icon={<MessagesIcon />}
+                            />
+                          </ListItem>
+                          <ListItem className="subtitle">
+                            <Label
+                              text={'0 devices registered'}
+                              icon={'fa fa-mobile'}
+                            />
+                          </ListItem>
+                        </List>
+                      </div>
+                    </DataListCell>,
+                  ]}
+                />
+              </DataListItemRow>
+            </DataListItem>
+          ))}
+        </DataList>
+      </>
+>>>>>>> Added application list page
     );
   }
 }
