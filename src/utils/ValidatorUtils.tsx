344b49b7369378de2d60586d1fc1b23aa68161e3
Massimiliano Ziccardi <mziccard@redhat.com>
2020-08-18 13:41:29 +0200

    refactor(validator): changed the utilites to accept null formvalitator
    Changed so that the accepter validation result object can be a null.
    This way it is very easy to reset the validation status when opening the
    modal

--- src/utils/ValidatorUtils.tsx
+++ src/utils/ValidatorUtils.tsx
@@ -1,14 +1,14 @@
 import { MultiEvaluationResult } from 'json-data-validator/build/src/Rule';
 
 export const validatorToPF4Status: (
-  status: MultiEvaluationResult | undefined,
+  status: MultiEvaluationResult | undefined | null,
   field: string
 ) => 'success' | 'error' | 'default' = (
-  status: MultiEvaluationResult | undefined,
+  status: MultiEvaluationResult | undefined | null,
   field: string
 ) => (status ? (status.isValid(field) ? 'success' : 'error') : 'default');
 
 export const formIsValid: (
-  status: MultiEvaluationResult | undefined
-) => boolean = (status: MultiEvaluationResult | undefined) =>
+  status: MultiEvaluationResult | undefined | null
+) => boolean = (status: MultiEvaluationResult | undefined | null) =>
   !!(status?.valid && status.valid);
