e7c4ccd9becc8228cb986c2e82ae116b2edda59e
Massimiliano Ziccardi <mziccard@redhat.com>
2020-08-24 16:56:06 +0200

    Adding keycloak integration

--- src/common/Loading.tsx
+++ src/common/Loading.tsx
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { EmptyState, Spinner, EmptyStateVariant } from '@patternfly/react-core';
 
-interface Props {
-  visible: boolean;
-}
-
-export class Loading extends Component<Props> {
+export class Loading extends Component<{}> {
   render() {
-    if (this.props.visible) {
-      return (
-        <EmptyState className={'emptyState'} variant={EmptyStateVariant.full}>
-          <Spinner />
-        </EmptyState>
-      );
-    } else {
-      return null;
-    }
+    return (
+      <EmptyState
+        variant={EmptyStateVariant.full}
+        style={{
+          marginTop: '64px',
+          marginLeft: '24px',
+          marginRight: '24px',
+        }}
+      >
+        <Spinner />
+      </EmptyState>
+    );
   }
 }
