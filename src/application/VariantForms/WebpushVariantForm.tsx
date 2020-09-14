b2bd3a2e30cb9329bf6d1f20750d2cb2d654ddd0
Massimiliano Ziccardi <mziccard@redhat.com>
2020-09-14 11:50:38 +0200

    fix: 🐛 fixed webpush alias validation
    The validation was configured only to check that a value was present.
    Now it checks that the value is a valid URL or mailto address too

--- src/application/VariantForms/WebpushVariantForm.tsx
+++ src/application/VariantForms/WebpushVariantForm.tsx
@@ -79,8 +79,25 @@ export class WebpushVariantForm extends Component<Props, State> {
       .withField('webpushAlias')
       .validate(
         RuleBuilder.required()
-          .withErrorMessage('Please enter a mailto or URL address')
+          .withErrorMessage('Please enter a valid URL or mailto address')
           .build()
+      )
+      .validate(
+        RuleBuilder.composite
+          .any()
+          .withSubRule(
+            RuleBuilder.matches(
+              '^mailto: ?(?:[a-z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])$'
+            )
+          )
+          .withSubRule(
+            RuleBuilder.isValidUrl()
+              .requireTld(true)
+              .requireHost(true)
+              .requireProtocol(true)
+              .build()
+          )
+          .build('Please enter a valid URL or mailto address')
       )
       .build();
 
